dfc1a911678210917d6733366ee0ca0d702901e2
jmb1079 <42720922+jmb1079@users.noreply.github.com>
2022-04-15 14:41:10 -0700

    Changed tutorial to introduce call to api

--- src/DecryptTranslateUi/Program.cs
+++ src/DecryptTranslateUi/Program.cs
@@ -20,6 +20,7 @@ builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddScoped<AuthenticationStateProvider, RevalidatingIdentityAuthenticationStateProvider<IdentityUser>>();
 builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddHttpClient();
 
 var app = builder.Build();
 
