19614b4da159fdca52508078624b5e9ed60bb078
jmb1079 <42720922+jmb1079@users.noreply.github.com>
2022-04-24 16:03:53 -0700

    Corrected type mismatch

--- src/Data/Investigator.cs
+++ src/Data/Investigator.cs
@@ -5,7 +5,7 @@ namespace DecryptTranslateUi.Data;
 public class Investigator
 {
     [JsonPropertyName("id")]
-    public int Id { get; set; }
+    public String Id { get; set; }
 
     [JsonPropertyName("name")]
     public String Name { get; set; }
